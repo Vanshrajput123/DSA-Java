9463b181254fce1bc2ae06d89f179396949004a9
Vanshrajput123 <151618988+Vanshrajput123@users.noreply.github.com>
2026-07-21 16:12:51 +0530

    Added LeetCode 189 Rotate Array

---
+++ Arrays/LC189_RotateArray.java
@@ -0,0 +1,57 @@
+ import java.util.Scanner;
+
+public class LC189_RotateArray {
+
+    public static void rotate(int[] nums, int k) {
+
+        k = k % nums.length;
+
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public static void reverse(int[] nums, int start, int end) {
+
+        while (start < end) {
+
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter value of k: ");
+        int k = sc.nextInt();
+
+        rotate(nums, k);
+
+        System.out.println("Rotated Array:");
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+
+        sc.close();
+    }
+} 
+    
+
