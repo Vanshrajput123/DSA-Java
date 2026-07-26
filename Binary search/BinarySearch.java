392b33157003225b68146d05a8462c61cff22581
Vanshrajput123 <151618988+Vanshrajput123@users.noreply.github.com>
2026-07-26 15:43:33 +0530

    Added Binary Search

---
+++ Binary search/BinarySearch.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class BinarySearch {
+
+    public static int search(int[] arr, int target) {
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter sorted array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target element: ");
+        int target = sc.nextInt();
+
+        int index = search(arr, target);
+
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found.");
+        }
+
+        sc.close();
+    }
+}
