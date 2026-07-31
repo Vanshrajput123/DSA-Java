4226afbe8cb60d7b4d7c4ba5cddbf306c433c8dc
Vanshrajput123 <151618988+Vanshrajput123@users.noreply.github.com>
2026-07-31 12:48:11 +0530

    Added String Palindrome Check

---
+++ String/IsPalindrome.java
@@ -0,0 +1,39 @@
+package String;
+import java.util.Scanner;
+
+public class IsPalindrome {
+
+    public static boolean isPalindrome(String str) {
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.next();
+
+        if (isPalindrome(str)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
+        sc.close();
+    }
+}
